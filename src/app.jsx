9e0d1aa0301d05cc5fa24a24d282985ec2065236
Saurav Ghosh <sauravghosh6215@gmail.com>
2024-05-05 01:09:03 +0530

    Started useEffect hook and completed dependency array concept in useEffect.

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,37 @@
 // import { LoginForm } from "./components/LoginForm";
 // import { RegisterForm } from "./components/RegisterForm";
-import { useState } from "react"
-import { UserDetails } from "./components/UserDetails";
+import { useEffect, useState } from "react"
+// import { UserDetails } from "./components/UserDetails";
+
 
 export default function App() {
-	const [username, setUsername] = useState("");	//used to create new username in add operation
-	const [email, setEmail] = useState("");			//used to create new email in add operation
-	const [counter, setCounter] = useState(3);		//used to assign an id by the system in add operation.(default value is set to 3 bcz array already has 2 elements)	
-	const [users, setUsers] = useState([			//used to perform edit operations in users array 
-		{
-			id: 1,
-			username: "anson",
-			email: "anson@gmail.com",
-		},
-		{
-			id: 2,
-			username: "saurav",
-			email: "saurav@gmail.com",
-		},
-	]);
+	const [counter, setCounter] = useState(0);
+	const [sync, setSync] = useState(false);
+
+	//useEffect runs whenever App is rendered
+	useEffect(
+		// () => {		//callback function will be called always wheverver App is rendered/re-rendered 
+		// 	document.title = "Saurav Developer"
+		// 	console.log("Rendering");
+		// })
+
+		// 	()=> {		//callback function will be called only once if empty dependency array '[]' is passed (effective in API fetching after first time loading)
+		// 	document.title = "Saurav Developer"
+		// 	console.log("Rendering");
+		// },[]) 
+
+		() => {		//callback function will be called whenevr there is a state update mentioned in dependency array
+			document.title = "Saurav Developer"+counter
+			console.log("Rendering");
+		}, [sync]); // should pass the state variables used insde useEffect in dependency array to always get the update value on re-rendering
+
+
 
 	return (
 		<div>
-			<form onSubmit={(e) => {
-				e.preventDefault();
-				const newUser = {
-					id: counter,
-					username,
-					email
-				};
-				setCounter((currentCounter) => currentCounter + 1);
-				setUsers((currentUsersState) => [...currentUsersState, newUser]);
-			}}>
-				<div>
-					<label htmlFor="username">Username</label>
-					<input name="username" id="username" value={username}
-						onChange={(e) => {
-							setUsername(e.target.value);
-						}} />
-				</div>
-				<div>
-					<label htmlFor="email">Email</label>
-					<input name="email" id="email"
-						onChange={(e) => {
-							setEmail(e.target.value)
-						}} />
-				</div>
-				<button>Add user</button>
-			</form>
-
-			{users.map((user) =>
-				(<UserDetails key={user.id} user={user} setUsers={setUsers} />) 	//never to use the index of array as key, it creates much problems and bugs while adding data like forms to be submit on backend. 
-			)}
-
+			<div>Clicked button {counter} times!</div>
+			<button onClick={() => setCounter((count) => count + 1)}>Click me</button>
+			<button onClick={() => { setSync((currentSync) => !currentSync) }}>sync</button>
 		</div>
 	)
 }
