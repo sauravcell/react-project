e8e16b821a68a3499f59cc2963114a8e3ffaa24b
Saurav Ghosh <sauravghosh6215@gmail.com>
2024-08-18 23:12:40 +0530

    In useEffect hook; completed concepts of fetching data, Abort controller, cleanup function.

--- src/app.jsx
+++ src/app.jsx
@@ -21,11 +21,37 @@ export default function App() {
 		// },[]) 
 
 		() => {		//callback function will be called whenevr there is a state update mentioned in dependency array
-			document.title = "Saurav Developer"+counter
+			document.title = "Saurav Developer" + counter
 			console.log("Rendering");
 		}, [sync]); // should pass the state variables used insde useEffect in dependency array to always get the update value on re-rendering
 
+	useEffect(() => {		//this use-effect hook is used to fetch daata using fetch api.
 
+		const controller = new AbortController();	//used to unmount component when required. It helps to cancel asyncrounous calls like API calls.
+		async function fetchUsers(){	
+			try {
+				console.log("fetching");
+				const response = await fetch(
+					'https://jsonplaceholder.typicode.com/users',
+					{signal: controller.signal}
+					);
+
+				const json =await response.json();		//response is returned aftr some time
+				console.log(json)
+
+			} 
+			catch (err) {
+				console.log(err);
+			}		
+		}
+		fetchUsers();	//invoking the async function for fetch api.
+
+		return ()=> {
+			controller.abort();   			//abort function  is invoked immediately if unmounting is going to happen which clears all pending asynchronous call.
+			console.log(controller.signal);		
+		}
+	})
+	
 
 	return (
 		<div>
